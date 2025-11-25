1ea0f42844bfe93c564087fbf6f6b67ad32b85a5
Marck-Code1 <marco.celis2005@gmail.com>
2025-11-25 07:51:12 -0600

    arreglar puerto 2

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
-const express = require('express');
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-const PORT = process.env.PORT || '3000'
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json()) // Esta funcion parsea el body de la peticion y si trae JSON lo va a agregar a la propiedad body
-app.use(cors())
+app.use(express.json()); // Esta funcion parsea el body de la peticion y si trae JSON lo va a agregar a la propiedad body
+app.use(cors());
 
-app.get('/alumnos',(req,res) => {
-    console.log(req.query)
-    res.send('Hola Mundo!');
+app.get("/alumnos", (req, res) => {
+  console.log(req.query);
+  res.send("Hola Mundo!");
 });
 
-app.get('/docentes/:control',(req,res) => {
-    console.log(req.params)
-    res.send('Hola Mundo!');
+app.get("/docentes/:control", (req, res) => {
+  console.log(req.params);
+  res.send("Hola Mundo!");
 });
 
-app.get('/directivos',(req,res) => {
-    console.log(req.body)
-    res.send('Hola Mundo!');
+app.get("/directivos", (req, res) => {
+  console.log(req.body);
+  res.send("Hola Mundo!");
 });
 
-
-app.post('/',(req,res) => {
-    res.send('Hola Mundo! post!!!!!');
+app.post("/", (req, res) => {
+  res.send("Hola Mundo! post!!!!!");
 });
 
-app.post('/directivos',(req,res) => {
-    console.log(req.body)
-    res.send('Hola Mundo! post!!!!!');
+app.post("/directivos", (req, res) => {
+  console.log(req.body);
+  res.send("Hola Mundo! post!!!!!");
 });
-
 
 app.listen(PORT, () => {
-    console.log('App listening on port 3000!')
+  console.log("App listening on port 3000!");
 });
